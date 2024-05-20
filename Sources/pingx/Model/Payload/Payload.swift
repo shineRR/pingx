2362828ec361b01a12973124f833af28688fe3d5
Ilya Baryka <barykoilyass@gmail.com>
2024-05-20 16:57:07 +0200

    Fixed payload id

--- Sources/pingx/Model/Payload/Payload.swift
+++ Sources/pingx/Model/Payload/Payload.swift
@@ -6,18 +6,18 @@ struct Payload {
     
     // MARK: Typealias
     
-    typealias ID = (UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8)
+    typealias PayloadID = (UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8, UInt8)
     
     // MARK: Properties
     
     // "pingx"
-    let identifier: ID
+    let identifier: PayloadID
     let timestamp: CFAbsoluteTime
     
     // MARK: Initializer
     
     init(
-        identifier: ID = (112, 105, 110, 103, 120, 0, 0, 0),
+        identifier: PayloadID = (112, 105, 110, 103, 120, 0, 0, 0),
         timestamp: CFAbsoluteTime = CFAbsoluteTimeGetCurrent()
     ) {
         self.identifier = identifier
